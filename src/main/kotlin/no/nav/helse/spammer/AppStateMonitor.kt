acf68073fa331433c38631a7881f4dcfdcd47dc3
David Steinsland <david@steinsland.email>
2020-04-17 08:47:26 +0200

    tilpasser event nytt format

--- src/main/kotlin/no/nav/helse/spammer/AppStateMonitor.kt
+++ src/main/kotlin/no/nav/helse/spammer/AppStateMonitor.kt
@@ -16,8 +16,11 @@ internal class AppStateMonitor(
     init {
         River(rapidsConnection).apply {
             validate { it.requireValue("@event_name", "app_status") }
-            validate { it.requireKey("states") }
-            validate { it.require("since", JsonNode::asLocalDateTime) }
+            validate { it.requireArray("states") {
+                requireKey("app", "state")
+                require("last_active_time", JsonNode::asLocalDateTime)
+            } }
+            validate { it.require("threshold", JsonNode::asLocalDateTime) }
             validate { it.require("@opprettet", JsonNode::asLocalDateTime) }
         }.register(this)
     }
@@ -27,23 +30,26 @@ internal class AppStateMonitor(
     override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) {
         if (lastReportTime > LocalDateTime.now().minusMinutes(2)) return // don't create alerts too eagerly
         val appsDown = packet["states"]
-            .fields()
-            .asSequence()
-            .filter { it.value.isIntegralNumber && it.value.asInt() == 0 }
-            .map { it.key }
-            .toList()
+            .filter { it["state"].asInt() == 0 }
+            .filter { it["last_active_time"].asLocalDateTime() < LocalDateTime.now().minusMinutes(2) }
+            .map { it["app"].asText() to it["last_active_time"].asLocalDateTime() }
 
         if (appsDown.isEmpty()) return
 
         val appString = appsDown.last().let { siste ->
-            if (appsDown.size == 1) siste
-            else appsDown.subList(0, appsDown.size - 1).joinToString() + " og $siste"
+            if (appsDown.size == 1) siste.printApp()
+            else appsDown.subList(0, appsDown.size - 1).joinToString { it.printApp() } + " og ${siste.printApp()}"
         }
         slackClient?.postMessage(String.format(
-            "%s har ikke svart på ping sendt %s siden.",
+            "%s er antatt nede fordi de ikke har svart på ping innen %s siden.",
             appString,
             humanReadableTime(ChronoUnit.SECONDS.between(packet["since"].asLocalDateTime(), LocalDateTime.now()))
         ))
         lastReportTime = LocalDateTime.now()
+    }
+
+    private fun Pair<String, LocalDateTime>.printApp(): String {
+        val tid = humanReadableTime(ChronoUnit.SECONDS.between(second, LocalDateTime.now()))
+        return "$first (sist aktiv: $tid siden)"
     }
 }
