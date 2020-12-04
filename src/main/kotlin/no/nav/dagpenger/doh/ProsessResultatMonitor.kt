38f21b65a911a1fe081657ef4b6176c30b9c54d3
André Roaldseth <andre.roaldseth@nav.no>
2020-12-04 13:58:06 +0100

    Legg til ProsessResultat hook for doh

---
+++ src/main/kotlin/no/nav/dagpenger/doh/ProsessResultatMonitor.kt
@@ -0,0 +1,48 @@
+package no.nav.dagpenger.doh
+
+import com.fasterxml.jackson.databind.JsonNode
+import mu.KotlinLogging
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageProblems
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
+import no.nav.helse.rapids_rivers.asLocalDateTime
+
+internal class ProsessResultatMonitor(
+    rapidsConnection: RapidsConnection,
+    private val slackClient: SlackClient?
+) : River.PacketListener {
+    companion object {
+        private val log = KotlinLogging.logger { }
+        private val sikkerlogg = KotlinLogging.logger("tjenestekall")
+    }
+
+    init {
+        River(rapidsConnection).apply {
+            validate {
+                it.demandValue("@event_name", "prosess_resultat")
+                it.requireKey("søknad_uuid", "resultat")
+                it.require("@opprettet", JsonNode::asLocalDateTime)
+            }
+        }.register(this)
+    }
+
+    override fun onPacket(packet: JsonMessage, context: RapidsConnection.MessageContext) {
+        slackClient?.postMessage(
+            text = String.format(
+                "Prosessen for <%s|%s> har blitt ferdig med resultatet %s",
+                Kibana.createUrl(
+                    String.format("\"%s\"", packet["søknad_uuid"].asText()),
+                    packet["@opprettet"].asLocalDateTime().minusHours(1)
+                ),
+                packet["søknad_uuid"].asText(),
+                packet["resultat"].asBoolean()
+            ),
+            emoji = ":tada:"
+        )
+    }
+
+    override fun onError(problems: MessageProblems, context: RapidsConnection.MessageContext) {
+        sikkerlogg.info(problems.toExtendedReport())
+    }
+}
