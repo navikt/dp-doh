c42bdf9ce1ecc493c3884622ba2e23f261083b41
rannveigskjerve <rannveig.alette.skjerve@nav.no>
2021-04-20 11:28:45 +0200

    Oppdater bibliotek versjon constants i tjenester
    Co-authored-by: Atle Nordland <atle.nordland@nav.no>

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -24,7 +24,7 @@ object Cucumber {
 object Dagpenger {
 
     object Biblioteker {
-        const val version = "2021.03.23-13.43.50d6860396fc"
+        const val version = "2021.04.20-10.27.57461935e892"
         const val stsKlient = "com.github.navikt.dp-biblioteker:sts-klient:$version"
         const val grunnbeløp = "com.github.navikt.dp-biblioteker:grunnbelop:$version"
         const val ktorUtils = "com.github.navikt.dp-biblioteker:ktor-utils:$version"
