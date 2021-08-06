80c37b8668746bdce6ce1c4648d1ae9306c9027e
André Roaldseth <andre.roaldseth@nav.no>
2021-08-06 09:40:28 +0200

    Bruk ny Slack-klient og ny melding for prosess_resultat også

--- src/main/kotlin/no/nav/dagpenger/doh/monitor/ProsessResultatMonitor.kt
+++ src/main/kotlin/no/nav/dagpenger/doh/monitor/ProsessResultatMonitor.kt
@@ -1,10 +1,12 @@
 package no.nav.dagpenger.doh.monitor
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.slack.api.methods.MethodsClient
+import com.slack.api.methods.kotlin_extension.request.chat.blocks
 import io.prometheus.client.Counter
 import mu.KotlinLogging
+import mu.withLoggingContext
 import no.nav.dagpenger.doh.Kibana
-import no.nav.dagpenger.doh.slack.SlackClient
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.MessageProblems
@@ -14,7 +16,8 @@ import no.nav.helse.rapids_rivers.asLocalDateTime
 
 internal class ProsessResultatMonitor(
     rapidsConnection: RapidsConnection,
-    private val slackClient: SlackClient?
+    private val slackClient: MethodsClient?,
+    private val slackChannelId: String
 ) : River.PacketListener {
     companion object {
         private val log = KotlinLogging.logger { }
@@ -37,20 +40,62 @@ internal class ProsessResultatMonitor(
     }
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        val uuid = packet["søknad_uuid"].asText()
+        val årsak = packet["seksjon_navn"].asText()
+
         resultatCounter.labels(packet["resultat"].asText()).inc()
 
-        slackClient?.postMessage(
-            text = String.format(
-                "Prosessen for <%s|%s> har blitt ferdig med resultatet %s",
-                Kibana.createUrl(
-                    String.format("\"%s\"", packet["søknad_uuid"].asText()),
+        withLoggingContext(
+            "søknad_uuid" to uuid
+        ) {
+            log.info { "Mottok manuell behandling" }
+
+            slackClient?.chatPostMessage {
+                val saksbehandlingslogg = Kibana.createUrl(
+                    String.format("\"%s\"", uuid),
                     packet["@opprettet"].asLocalDateTime().minusHours(1)
-                ),
-                packet["søknad_uuid"].asText(),
-                packet["resultat"].asBoolean()
-            ),
-            emoji = ":tada:"
-        )
+                )
+                val resultat = if (packet["resultat"].asBoolean())
+                    "Automatisk innvilgelse" else "Automatisk avslag :no_entry_sign:"
+
+                it.channel(slackChannelId)
+                    .blocks {
+                        section {
+                            plainText(":checkered_flag: Jeg har saksbehandlet en søknad!")
+                        }
+                        section {
+                            markdownText("*Resultat:* $resultat\n")
+                        }
+                        section {
+                            markdownText(
+                                listOf(
+                                    "*UUID*: $uuid",
+                                    "**: $årsak",
+                                ).joinToString("\n")
+                            )
+                        }
+                        actions {
+                            button {
+                                text(":ledger: Se saksbehandlingslogg")
+                                url(saksbehandlingslogg)
+                            }
+                        }
+                    }
+                    .text(
+                        String.format(
+                            "På grunn av %s kan ikke søknaden %s automatiseres, den går til manuell behandling i Arena",
+                            årsak,
+                            uuid,
+                        )
+                    )
+                    .iconEmoji(":robot_face:")
+                    .username("dp-quiz")
+            }?.let { response ->
+                if (!response.isOk) {
+                    log.error { "Kunne ikke poste på Slack fordi ${response.error}" }
+                }
+            }
+        }
     }
 
     override fun onError(problems: MessageProblems, context: MessageContext) {
