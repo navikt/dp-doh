190c15acb01fe84f959a58f5fdfb4830fa6d2287
André Roaldseth <andre.roaldseth@nav.no>
2022-01-13 14:00:35 +0100

    Legg til monitor som logger fordeling av inntekter

---
+++ src/main/kotlin/no/nav/dagpenger/doh/monitor/InntektsMonitor.kt
@@ -0,0 +1,62 @@
+package no.nav.dagpenger.doh.monitor
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.ktor.utils.io.core.use
+import io.prometheus.client.Histogram
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.RapidsConnection
+import no.nav.helse.rapids_rivers.River
+import no.nav.helse.rapids_rivers.asLocalDateTime
+import java.net.URL
+
+// Logger fordeling av inntekt på vedtakene som går gjennom quiz.
+internal class InntektsMonitor(
+    rapidsConnection: RapidsConnection,
+) : River.PacketListener {
+    companion object {
+        private val objectMapper = ObjectMapper()
+        private val inntektsteller =
+            Histogram.build("dp_inntekt", "Inntekt for automatisering").labelNames("inntektsgruppe", "type")
+                .linearBuckets(0.0, 50000.0, 6).register()
+        private val gjeldendeGrunnbeløp by lazy {
+            URL("https://g.nav.no/api/v1/grunnbeloep").openStream().bufferedReader().use {
+                objectMapper.readTree(it).run {
+                    this["grunnbeloep"].asDouble()
+                }
+            }
+        }
+    }
+
+    init {
+        River(rapidsConnection).apply {
+            validate {
+                it.demandValue("@event_name", "prosess_resultat")
+                it.requireKey("søknad_uuid", "resultat")
+                it.require("@opprettet", JsonNode::asLocalDateTime)
+                it.requireKey("fakta")
+            }
+        }.register(this)
+    }
+
+    override fun onPacket(packet: JsonMessage, context: MessageContext) {
+        packet["fakta"].filter { it["type"].asText() == "inntekt" }.forEach {
+            val inntekt = it["svar"].asDouble()
+            inntektsteller.labels(
+                inntektsgruppe(inntekt),
+                it["navn"].asText()
+            ).observe(inntekt)
+        }
+    }
+
+    private fun inntektsgruppe(inntekt: Number): String {
+        return when (inntekt.toDouble()) {
+            in 0.G..3.G -> "0-3G"
+            in 3.G..6.G -> "3-6G"
+            else -> ">6G"
+        }
+    }
+
+    private val Int.G get() = this * gjeldendeGrunnbeløp
+}
---
+++ src/test/kotlin/no/nav/dagpenger/doh/InntektsMonitorTest.kt
@@ -0,0 +1,242 @@
+package no.nav.dagpenger.doh
+
+import io.prometheus.client.CollectorRegistry
+import no.nav.dagpenger.doh.monitor.InntektsMonitor
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class InntektsMonitorTest {
+    private val registry: CollectorRegistry = CollectorRegistry.defaultRegistry
+    private val rapid by lazy {
+        TestRapid().apply {
+            InntektsMonitor(this)
+        }
+    }
+
+    @AfterEach
+    fun cleanUp() {
+        rapid.reset()
+    }
+
+    @Test
+    fun `skal poste måle inntekt i Prometheus`() {
+        rapid.sendTestMessage(resultatJSON)
+
+        registry.getSampleValue(
+            "dp_inntekt_count",
+            listOf(
+                "inntektsgruppe",
+                "type",
+            ).toTypedArray(),
+            listOf(
+                "0-3G",
+                "Inntekt siste 36 mnd"
+            ).toTypedArray()
+        ).also {
+            assertEquals(it, 3.0)
+        }
+    }
+}
+
+@Language("JSON")
+private val resultatJSON = """{
+  "@event_name": "prosess_resultat",
+  "@opprettet": "2021-07-13T09:58:33.532432",
+  "@id": "35d6da40-44a6-4774-a2a9-d5543374b714",
+  "søknad_uuid": "6c7cd545-a06c-4769-b401-31795637a381",
+  "resultat": false,
+  "saksbehandles_på_ekte": true,
+  "identer": [
+    {
+      "id": "12345678910",
+      "type": "folkeregisterident",
+      "historisk": false
+    }
+  ],
+  "fakta": [
+    {
+      "navn": "Ønsker dagpenger fra dato",
+      "id": "1",
+      "roller": [
+        "nav"
+      ],
+      "type": "localdate",
+      "godkjenner": [],
+      "svar": "2018-01-05"
+    },
+    {
+      "navn": "Inntekt siste 36 mnd",
+      "id": "6",
+      "roller": [
+        "nav"
+      ],
+      "type": "inntekt",
+      "godkjenner": [],
+      "svar": 2000000.0
+    },
+    {
+      "navn": "Inntekt siste 36 mnd",
+      "id": "6",
+      "roller": [
+        "nav"
+      ],
+      "type": "inntekt",
+      "godkjenner": [],
+      "svar": 2000.0
+    },
+    {
+      "navn": "Inntekt siste 36 mnd",
+      "id": "6",
+      "roller": [
+        "nav"
+      ],
+      "type": "inntekt",
+      "godkjenner": [],
+      "svar": 20000.0
+    },
+    {
+      "navn": "Inntekt siste 36 mnd",
+      "id": "6",
+      "roller": [
+        "nav"
+      ],
+      "type": "inntekt",
+      "godkjenner": [],
+      "svar": 200000.0
+    },
+    {
+      "navn": "Søknadstidspunkt",
+      "id": "13",
+      "roller": [
+        "nav"
+      ],
+      "type": "localdate",
+      "godkjenner": [],
+      "svar": "2018-01-02"
+    },
+    {
+      "navn": "Innsendt søknadsId",
+      "id": "17",
+      "roller": [],
+      "type": "dokument",
+      "godkjenner": [],
+      "svar": {
+        "lastOppTidsstempel": "2021-07-13T09:58:33.490810",
+        "url": "ABCD123"
+      }
+    },
+    {
+      "navn": "Behandlingsdato",
+      "id": "23",
+      "roller": [
+        "nav"
+      ],
+      "type": "localdate",
+      "godkjenner": [],
+      "svar": "2018-01-05"
+    },
+    {
+      "navn": "Ordinær",
+      "id": "27.1",
+      "roller": [
+        "nav"
+      ],
+      "type": "boolean",
+      "godkjenner": [],
+      "svar": false
+    },
+    {
+      "navn": "Permittert",
+      "id": "28.1",
+      "roller": [
+        "nav"
+      ],
+      "type": "boolean",
+      "godkjenner": [],
+      "svar": true
+    },
+    {
+      "navn": "Lønnsgaranti",
+      "id": "29.1",
+      "roller": [
+        "nav"
+      ],
+      "type": "boolean",
+      "godkjenner": [],
+      "svar": false
+    },
+    {
+      "navn": "PermittertFiskeforedling",
+      "id": "30.1",
+      "roller": [
+        "nav"
+      ],
+      "type": "boolean",
+      "godkjenner": [],
+      "svar": false
+    },
+    {
+      "navn": "FagsakId i Arena",
+      "id": "52",
+      "roller": [],
+      "type": "dokument",
+      "godkjenner": [],
+      "svar": {
+        "lastOppTidsstempel": "2021-07-13T09:58:33.488590",
+        "url": "123123"
+      }
+    },
+    {
+      "navn": "Over 67 år fra-dato",
+      "id": "55",
+      "roller": [
+        "nav"
+      ],
+      "type": "localdate",
+      "godkjenner": [],
+      "svar": "2018-01-01"
+    },
+    {
+      "navn": "Virkningsdato",
+      "id": "4",
+      "roller": [],
+      "type": "localdate",
+      "godkjenner": [],
+      "svar": "2018-01-05"
+    },
+    {
+      "navn": "Første dato av virkningsdato og behandlingsdato",
+      "id": "45",
+      "roller": [],
+      "type": "localdate",
+      "godkjenner": [],
+      "svar": "2018-01-05"
+    }
+  ],
+  "subsumsjoner": [
+    {
+      "lokalt_resultat": true,
+      "navn": "Sjekk at `sluttårsaker med id 26` er lik 1",
+      "forklaring": "saksbehandlerforklaring",
+      "type": "Enkel subsumsjon"
+    },
+    {
+      "lokalt_resultat": false,
+      "navn": "under 67år",
+      "type": "Deltre subsumsjon",
+      "forklaring": "saksbehandlerforklaring",
+      "subsumsjoner": [
+        {
+          "lokalt_resultat": false,
+          "navn": "Sjekk at 'Virkningsdato med id 4' er før 'Over 67 år fra-dato med id 55'",
+          "forklaring": "saksbehandlerforklaring",
+          "type": "Enkel subsumsjon"
+        }
+      ]
+    }
+  ]
+}
+""".trimIndent()
