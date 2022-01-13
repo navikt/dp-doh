f0ad574291b138abc39a2770f750e9341d95c6d4
André Roaldseth <andre.roaldseth@nav.no>
2022-01-13 18:44:12 +0100

    Fiks måten vi finner inntekt i løsninga

--- src/main/kotlin/no/nav/dagpenger/doh/monitor/InntektsMonitor.kt
+++ src/main/kotlin/no/nav/dagpenger/doh/monitor/InntektsMonitor.kt
@@ -3,10 +3,8 @@ package no.nav.dagpenger.doh.monitor
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.ktor.utils.io.core.use
 import io.prometheus.client.Histogram
-import mu.KotlinLogging
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
-import no.nav.helse.rapids_rivers.MessageProblems
 import no.nav.helse.rapids_rivers.RapidsConnection
 import no.nav.helse.rapids_rivers.River
 import java.net.URL
@@ -17,7 +15,6 @@ internal class InntektsMonitor(
 ) : River.PacketListener {
     companion object {
         private val objectMapper = ObjectMapper()
-        private val sikkerlogg = KotlinLogging.logger("tjenestekall")
         private val inntektsteller =
             Histogram.build("dp_inntekt", "Inntekt for automatisering").labelNames("inntektsgruppe", "type")
                 .linearBuckets(0.0, 50000.0, 12).register()
@@ -35,28 +32,17 @@ internal class InntektsMonitor(
             validate {
                 it.demandValue("@event_name", "faktum_svar")
                 it.requireAllOrAny("@behov", listOf("InntektSiste12Mnd", "InntektSiste3År"))
-                it.requireKey("fakta")
                 it.requireKey("@løsning")
             }
         }.register(this)
     }
 
-    override fun onError(problems: MessageProblems, context: MessageContext) {
-        sikkerlogg.info { problems.toExtendedReport() }
-    }
-
-    override fun onSevere(error: MessageProblems.MessageException, context: MessageContext) {
-        sikkerlogg.info { error.problems }
-    }
-
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
-        sikkerlogg.info { "logget inntekt for ${packet.toJson()}" }
-        packet["fakta"].filter { it["type"].asText() == "inntekt" }.forEach {
-            val inntekt = it["svar"].asDouble()
-            inntektsteller.labels(
-                inntektsgruppe(inntekt),
-                it["navn"].asText()
-            ).observe(inntekt)
+        packet["@løsning"].fields().forEach {
+            val navn: String = it.key
+            val inntekt: Double = it.value.asDouble()
+
+            inntektsteller.labels(inntektsgruppe(inntekt), navn).observe(inntekt)
         }
     }
 
--- src/test/kotlin/no/nav/dagpenger/doh/InntektsMonitorTest.kt
+++ src/test/kotlin/no/nav/dagpenger/doh/InntektsMonitorTest.kt
@@ -32,11 +32,11 @@ internal class InntektsMonitorTest {
                 "type",
             ).toTypedArray(),
             listOf(
-                "0-3G",
-                "Inntekt siste 36 mnd"
+                "3-6G",
+                "InntektSiste3År"
             ).toTypedArray()
         ).also {
-            assertEquals(it, 3.0)
+            assertEquals(it, 1.0)
         }
     }
 }
@@ -44,401 +44,46 @@ internal class InntektsMonitorTest {
 @Language("JSON")
 private val behovJSON = """{
   "@event_name": "faktum_svar",
-  "@behov": ["InntektSiste12Mnd", "InntektSiste3År"],
-  "@løsning": [],
-  "@opprettet": "2021-07-13T09:58:33.532432",
-  "@id": "35d6da40-44a6-4774-a2a9-d5543374b714",
-  "søknad_uuid": "6c7cd545-a06c-4769-b401-31795637a381",
-  "resultat": false,
-  "saksbehandles_på_ekte": true,
-  "identer": [
-    {
-      "id": "12345678910",
-      "type": "folkeregisterident",
-      "historisk": false
-    }
-  ],
+  "@opprettet": "2022-01-13T15:03:12.274510120",
+  "@id": "b4d15915-e89a-4e6f-a968-a6d1caa3fd90",
+  "søknad_uuid": "01acedb6-1136-4bbb-b394-d799eb0b2d67",
+  "seksjon_navn": "inntekter",
   "fakta": [
     {
-      "navn": "Ønsker dagpenger fra dato",
-      "id": "1",
-      "roller": [
-        "nav"
-      ],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-05"
-    },
-    {
-      "navn": "Inntekt siste 36 mnd",
       "id": "6",
-      "roller": [
-        "nav"
-      ],
-      "type": "inntekt",
-      "godkjenner": [],
-      "svar": 2000000.0
+      "behov": "InntektSiste3År",
+      "clazz": "inntekt"
     },
     {
-      "navn": "Inntekt siste 36 mnd",
-      "id": "6",
-      "roller": [
-        "nav"
-      ],
-      "type": "inntekt",
-      "godkjenner": [],
-      "svar": 2000.0
-    },
-    {
-      "navn": "Inntekt siste 36 mnd",
-      "id": "6",
-      "roller": [
-        "nav"
-      ],
-      "type": "inntekt",
-      "godkjenner": [],
-      "svar": 20000.0
-    },
-    {
-      "navn": "Inntekt siste 36 mnd",
-      "id": "6",
-      "roller": [
-        "nav"
-      ],
-      "type": "inntekt",
-      "godkjenner": [],
-      "svar": 200000.0
-    },
-    {
-      "navn": "Søknadstidspunkt",
-      "id": "13",
-      "roller": [
-        "nav"
-      ],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-02"
-    },
-    {
-      "navn": "Innsendt søknadsId",
-      "id": "17",
-      "roller": [],
-      "type": "dokument",
-      "godkjenner": [],
-      "svar": {
-        "lastOppTidsstempel": "2021-07-13T09:58:33.490810",
-        "url": "ABCD123"
-      }
-    },
-    {
-      "navn": "Behandlingsdato",
-      "id": "23",
-      "roller": [
-        "nav"
-      ],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-05"
-    },
-    {
-      "navn": "Ordinær",
-      "id": "27.1",
-      "roller": [
-        "nav"
-      ],
-      "type": "boolean",
-      "godkjenner": [],
-      "svar": false
-    },
-    {
-      "navn": "Permittert",
-      "id": "28.1",
-      "roller": [
-        "nav"
-      ],
-      "type": "boolean",
-      "godkjenner": [],
-      "svar": true
-    },
-    {
-      "navn": "Lønnsgaranti",
-      "id": "29.1",
-      "roller": [
-        "nav"
-      ],
-      "type": "boolean",
-      "godkjenner": [],
-      "svar": false
-    },
-    {
-      "navn": "PermittertFiskeforedling",
-      "id": "30.1",
-      "roller": [
-        "nav"
-      ],
-      "type": "boolean",
-      "godkjenner": [],
-      "svar": false
-    },
-    {
-      "navn": "FagsakId i Arena",
-      "id": "52",
-      "roller": [],
-      "type": "dokument",
-      "godkjenner": [],
-      "svar": {
-        "lastOppTidsstempel": "2021-07-13T09:58:33.488590",
-        "url": "123123"
-      }
-    },
-    {
-      "navn": "Over 67 år fra-dato",
-      "id": "55",
-      "roller": [
-        "nav"
-      ],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-01"
-    },
-    {
-      "navn": "Virkningsdato",
-      "id": "4",
-      "roller": [],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-05"
-    },
-    {
-      "navn": "Første dato av virkningsdato og behandlingsdato",
-      "id": "45",
-      "roller": [],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-05"
+      "id": "7",
+      "behov": "InntektSiste12Mnd",
+      "clazz": "inntekt"
     }
   ],
-  "subsumsjoner": [
+  "@behov": [
+    "InntektSiste3År",
+    "InntektSiste12Mnd"
+  ],
+  "Virkningstidspunkt": "2022-01-13",
+  "FangstOgFiskeInntektSiste36mnd": false,
+  "ØnskerDagpengerFraDato": "2022-01-01",
+  "Søknadstidspunkt": "2022-01-13",
+  "system_read_count": 1,
+  "system_participating_services": [
     {
-      "lokalt_resultat": true,
-      "navn": "Sjekk at `sluttårsaker med id 26` er lik 1",
-      "forklaring": "saksbehandlerforklaring",
-      "type": "Enkel subsumsjon"
+      "service": "dp-oppslag-inntekt",
+      "instance": "dp-oppslag-inntekt-84c69b97d-nvvcf",
+      "time": "2022-01-13T15:03:12.288632"
     },
     {
-      "lokalt_resultat": false,
-      "navn": "under 67år",
-      "type": "Deltre subsumsjon",
-      "forklaring": "saksbehandlerforklaring",
-      "subsumsjoner": [
-        {
-          "lokalt_resultat": false,
-          "navn": "Sjekk at 'Virkningsdato med id 4' er før 'Over 67 år fra-dato med id 55'",
-          "forklaring": "saksbehandlerforklaring",
-          "type": "Enkel subsumsjon"
-        }
-      ]
-    }
-  ]
-}
-""".trimIndent()
-
-@Language("JSON")
-private val resultatJSON = """{
-  "@event_name": "prosess_resultat",
-  "@opprettet": "2021-07-13T09:58:33.532432",
-  "@id": "35d6da40-44a6-4774-a2a9-d5543374b714",
-  "søknad_uuid": "6c7cd545-a06c-4769-b401-31795637a381",
-  "resultat": false,
-  "saksbehandles_på_ekte": true,
-  "identer": [
-    {
-      "id": "12345678910",
-      "type": "folkeregisterident",
-      "historisk": false
+      "service": "dp-doh",
+      "instance": "dp-doh-7dcc747f98-j49dc",
+      "time": "2022-01-13T14:04:38.686890519"
     }
   ],
-  "fakta": [
-    {
-      "navn": "Ønsker dagpenger fra dato",
-      "id": "1",
-      "roller": [
-        "nav"
-      ],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-05"
-    },
-    {
-      "navn": "Inntekt siste 36 mnd",
-      "id": "6",
-      "roller": [
-        "nav"
-      ],
-      "type": "inntekt",
-      "godkjenner": [],
-      "svar": 2000000.0
-    },
-    {
-      "navn": "Inntekt siste 36 mnd",
-      "id": "6",
-      "roller": [
-        "nav"
-      ],
-      "type": "inntekt",
-      "godkjenner": [],
-      "svar": 2000.0
-    },
-    {
-      "navn": "Inntekt siste 36 mnd",
-      "id": "6",
-      "roller": [
-        "nav"
-      ],
-      "type": "inntekt",
-      "godkjenner": [],
-      "svar": 20000.0
-    },
-    {
-      "navn": "Inntekt siste 36 mnd",
-      "id": "6",
-      "roller": [
-        "nav"
-      ],
-      "type": "inntekt",
-      "godkjenner": [],
-      "svar": 200000.0
-    },
-    {
-      "navn": "Søknadstidspunkt",
-      "id": "13",
-      "roller": [
-        "nav"
-      ],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-02"
-    },
-    {
-      "navn": "Innsendt søknadsId",
-      "id": "17",
-      "roller": [],
-      "type": "dokument",
-      "godkjenner": [],
-      "svar": {
-        "lastOppTidsstempel": "2021-07-13T09:58:33.490810",
-        "url": "ABCD123"
-      }
-    },
-    {
-      "navn": "Behandlingsdato",
-      "id": "23",
-      "roller": [
-        "nav"
-      ],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-05"
-    },
-    {
-      "navn": "Ordinær",
-      "id": "27.1",
-      "roller": [
-        "nav"
-      ],
-      "type": "boolean",
-      "godkjenner": [],
-      "svar": false
-    },
-    {
-      "navn": "Permittert",
-      "id": "28.1",
-      "roller": [
-        "nav"
-      ],
-      "type": "boolean",
-      "godkjenner": [],
-      "svar": true
-    },
-    {
-      "navn": "Lønnsgaranti",
-      "id": "29.1",
-      "roller": [
-        "nav"
-      ],
-      "type": "boolean",
-      "godkjenner": [],
-      "svar": false
-    },
-    {
-      "navn": "PermittertFiskeforedling",
-      "id": "30.1",
-      "roller": [
-        "nav"
-      ],
-      "type": "boolean",
-      "godkjenner": [],
-      "svar": false
-    },
-    {
-      "navn": "FagsakId i Arena",
-      "id": "52",
-      "roller": [],
-      "type": "dokument",
-      "godkjenner": [],
-      "svar": {
-        "lastOppTidsstempel": "2021-07-13T09:58:33.488590",
-        "url": "123123"
-      }
-    },
-    {
-      "navn": "Over 67 år fra-dato",
-      "id": "55",
-      "roller": [
-        "nav"
-      ],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-01"
-    },
-    {
-      "navn": "Virkningsdato",
-      "id": "4",
-      "roller": [],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-05"
-    },
-    {
-      "navn": "Første dato av virkningsdato og behandlingsdato",
-      "id": "45",
-      "roller": [],
-      "type": "localdate",
-      "godkjenner": [],
-      "svar": "2018-01-05"
-    }
-  ],
-  "subsumsjoner": [
-    {
-      "lokalt_resultat": true,
-      "navn": "Sjekk at `sluttårsaker med id 26` er lik 1",
-      "forklaring": "saksbehandlerforklaring",
-      "type": "Enkel subsumsjon"
-    },
-    {
-      "lokalt_resultat": false,
-      "navn": "under 67år",
-      "type": "Deltre subsumsjon",
-      "forklaring": "saksbehandlerforklaring",
-      "subsumsjoner": [
-        {
-          "lokalt_resultat": false,
-          "navn": "Sjekk at 'Virkningsdato med id 4' er før 'Over 67 år fra-dato med id 55'",
-          "forklaring": "saksbehandlerforklaring",
-          "type": "Enkel subsumsjon"
-        }
-      ]
-    }
-  ]
+  "@løsning": {
+    "InntektSiste3År": 398571.08,
+    "InntektSiste12Mnd": 274792.8
+  }
 }
 """.trimIndent()
