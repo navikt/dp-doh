2959a6eb32a70e062b8430900a86859f708e31e6
Geir André Lund <geir.andre.lund@nav.no>
2024-09-11 13:41:11 +0200

    Unngå å telle republiseringer fra dp-saksbehandling

--- src/main/kotlin/no/nav/dagpenger/doh/monitor/behandling/BehandlingStatusMonitor.kt
+++ src/main/kotlin/no/nav/dagpenger/doh/monitor/behandling/BehandlingStatusMonitor.kt
@@ -24,6 +24,7 @@ internal class BehandlingStatusMonitor(
                         "@event_name",
                         listOf("behandling_opprettet", "forslag_til_vedtak", "vedtak_fattet", "behandling_avbrutt"),
                     )
+                    it.requireKey("meldingOmVedtakProdusent") // Unngå å telle republiseringer fra dp-saksbehandling
                     it.requireKey("behandlingId", "gjelderDato", "søknadId")
                     it.interestedIn("@opprettet", "årsak", "avklaringer", "utfall", "automatisk")
                 }
