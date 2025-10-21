d8f042465d1e5d16faf6db60afd30b6214a6f788
Geir André Lund <geir.andre.lund@nav.no>
2025-10-21 14:09:58 +0200

    skriveleif

--- src/main/kotlin/no/nav/dagpenger/doh/monitor/behandling/BeregnMeldekortMottak.kt
+++ src/main/kotlin/no/nav/dagpenger/doh/monitor/behandling/BeregnMeldekortMottak.kt
@@ -39,7 +39,7 @@ internal class BeregnMeldekortMottak(
             """
             |Skal beregne meldekort
             |*Meldekort ID*: $eksternMeldekortId
-            |(intent meldekort ID: $meldekortId)
+            |(intern meldekort ID: $meldekortId)
             """.trimMargin()
 
         withLoggingContext(
