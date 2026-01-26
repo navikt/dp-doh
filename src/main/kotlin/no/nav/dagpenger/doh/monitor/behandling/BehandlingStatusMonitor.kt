acb7d008d63240f1852adb50083b8a2b2b86e961
Geir André Lund <geir.andre.lund@nav.no>
2026-01-26 14:40:16 +0100

    Revert "Skipper behandling i dev."
    This reverts commit 7bece854a0b4cf2d62d10cae4406cb8edd5b0329.

--- src/main/kotlin/no/nav/dagpenger/doh/monitor/behandling/BehandlingStatusMonitor.kt
+++ src/main/kotlin/no/nav/dagpenger/doh/monitor/behandling/BehandlingStatusMonitor.kt
@@ -47,12 +47,6 @@ internal class BehandlingStatusMonitor(
     ) {
         val behandlingId = packet["behandlingId"].asText()
 
-        if (behandlingId == "019bf98f-a7ea-7464-b00b-f55895d75c8b") {
-            // Midlertidig filter for å unngå støy fra en spesifikk behandling som ikke er laget fra dp-behandling i dev?
-            logger.info { "Ignorerer behandling med ID $behandlingId" }
-            return
-        }
-
         val behandletHendelse =
             BehandletHendelse(
                 id = packet["behandletHendelse"]["id"].asText(),
@@ -70,15 +64,9 @@ internal class BehandlingStatusMonitor(
             val status =
                 when (eventName) {
                     "behandling_avbrutt" -> Status.BEHANDLING_AVBRUTT
-
-                    "forslag_til_behandlingsresultat" -> return
-
-                    // Vi vil bare telle, ikke poste Slack-melding
+                    "forslag_til_behandlingsresultat" -> return // Vi vil bare telle, ikke poste Slack-melding
                     "behandlingsresultat" -> Status.VEDTAK_FATTET
-
-                    "behandling_opprettet" -> return
-
-                    // Vi vil bare telle, ikke poste Slack-melding
+                    "behandling_opprettet" -> return // Vi vil bare telle, ikke poste Slack-melding
                     else -> return
                 }
 
